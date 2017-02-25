f1f345904fb5e2de7d53373518cc2edcb5c67781
CorsoOleg <corso441@gmail.com>
2017-02-25 17:59:59 +0200

    new cod

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,36 @@
-var ball = document.querySelector(".discoball");
-var classes = ["here","goes","our","awesome","disco","ball"]
+/*var ball = document.querySelector(".discoball");
+var classes = "here","goes","our","awesome","disco","ball".split(" ");
 var i = 0;
 
 function goDisco() {
 	//do your disco magic here !
 	ball.addAttribute("class","discoball " + classes(i));
-i++;
-if (i >= 5) {
+    i++;
+    if (i >= 5) {
 	i = 0;
-}
+   }
 }
 
-setInterval(goDisco,300);
+setInterval(goDisco,300);*/
+
+
+var ball = document.querySelector(".discoball");
+
+var classes = ["here", "goes", "our", "awesome", "disco", "ball"];
+
+var i = 0;
+
+//function goDisco() {
+//	var random = Math.floor((Math.random() * classes.length));
+//	ball.setAttribute("class", "discoball " + classes[random]);
+//}
+
+//setInterval(goDisco,300);
+
+function goDiscoTrue() {
+		ball.setAttribute("class", "discoball " + classes[i]);
+		i++;
+		if (i > 5)	{i = 0;}
+}
+
+setInterval(goDiscoTrue,300);
