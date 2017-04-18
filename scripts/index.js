6ae66aef06e2ba46400019e9af4a152a772bc020
Legolas116 <legolas116@yandex.ru>
2017-04-18 19:54:38 +0300

    Ready

--- scripts/index.js
+++ scripts/index.js
@@ -4,13 +4,6 @@ var classes = ["here", "goes", "our", "awesome", "disco", "ball"];
 
 var i = 0;
 
-//function goDisco() {
-//	var random = Math.floor((Math.random() * classes.length));
-//	ball.setAttribute("class", "discoball " + classes[random]);
-//}
-
-//setInterval(goDisco,300);
-
 function goDiscoTrue() {
 		ball.setAttribute("class", "discoball " + classes[i]);
 		i++;
